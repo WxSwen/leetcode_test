cd6c641dd9f872d8e0046500a4270ca47923fcbc
WxSwen <511854671@qq.com>
2018-10-11 16:49:17 +0800

    feat:三数相加0

--- 15.js
+++ 15.js
@@ -1,24 +1,45 @@
+// Example:
+
+// Given array nums = [-1, 0, 1, 2, -1, -4],
+
+// A solution set is:
+// [
+//   [-1, 0, 1],
+//   [-1, -1, 2]
+// ]
+
 /**
  * @param {number[]} nums
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    var sumArray = [];
-    var unique = [];
-    var i = 0;
-    while(i < nums.length - 2) {
-      for(var j = i + 1; j < nums.length - 1; j++){
-        for(var k = j + 1; k < nums.length; k++) {
-          if(0 - nums[i] - nums[j] === nums[k]) {
-            if(nums.indexOf(nums[k]) >= k){
-              sumArray.push([nums[i], nums[j], nums[k]]);
-            }
-          }
-        }
-      }
-      i++;
+  nums.sort((a, b) => a - b);
+  var res = [];
+  var n = nums.length;
+  
+  for(var i = 0; i < n - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue;
     }
-    return sumArray;
+    for(var j = i + 1, k = n - 1; j < k;) {
+      if(nums[i] + nums[j] + nums[k] === 0) {
+        res.push([ nums[i], nums[j], nums[k] ])
+        j++;
+        k--;
+        while(j < k && nums[j] === nums[j - 1]) {
+          j++;
+        }
+        while(j < k && nums[k] === nums[k + 1]) {
+          k--;
+        }
+      } else if(nums[i] + nums[j] + nums[k] > 0){
+        k--;
+      } else {
+        j++;
+      }
+    }
+  }
+  return res;
 };
 
 console.log(threeSum([-1, 0, 1, 2, -1, -4]));
