c83dd4bcb5e507319ff92d4db1bcc884c3dbb3b7
WxSwen <511854671@qq.com>
2018-10-24 14:42:57 +0800

    feat:求取全排列

---
+++ 30.js
@@ -0,0 +1,33 @@
+// /**
+//  * @param {string} s
+//  * @param {string[]} words
+//  * @return {number[]}
+//  */
+// var findSubstring = function(s, words) {
+//     var set = new Set();
+
+    
+// };
+
+function permutate(str) {
+    var result = [];
+ 
+    if(str.length > 1) {
+        var left = str[0];
+        var rest = str.slice(1, str.length);
+        var preResult = permutate(rest);
+
+        for(var i=0; i<preResult.length; i++) {
+            for(var j=0; j<preResult[i].length + 1; j++) {
+                var tmp = preResult[i].slice(0, j) + left + preResult[i].slice(j, preResult[i].length);
+                
+                result.push(tmp);
+            }
+        }
+    } else if (str.length == 1) {
+        return [str];
+    }
+    return result;
+}
+
+permutate('abc');
