790a68517820a25da0f577b84cfe663ec79563ba
WxSwen <511854671@qq.com>
2018-12-14 21:01:22 +0800

    搜索二叉树递归遍历

---
+++ 236. Lowest Common Ancestor of a Binary Tree.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+// 递归遍历 p/q最近的节点 是第一个root节点
+// 时间复杂度O(n)
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    if(root === null || root === p || root === q) return root;
+    let left = lowestCommonAncestor(root.left, p, q);
+    let right = lowestCommonAncestor(root.right, p, q);
+    return left === null ? right : right === null ? left : root;
+};
--- 239(Sliding Window Maximum).js
+++ 239(Sliding Window Maximum).js
@@ -22,17 +22,17 @@
 //     return res;
 // };
 
-const findMax = (arr) => {
-  let max = arr[0];
-  let index = 0;
-  for (let i = 1; i < arr.length; i += 1) {
-      if (arr[i] > max) {
-          max = arr[i];
-          index = i;
-      }
-  }
-  return {max: max, index: index};
-}
+// const findMax = (arr) => {
+//   let max = arr[0];
+//   let index = 0;
+//   for (let i = 1; i < arr.length; i += 1) {
+//       if (arr[i] > max) {
+//           max = arr[i];
+//           index = i;
+//       }
+//   }
+//   return {max: max, index: index};
+// }
 var maxSlidingWindow = function(nums, k) {
   if (nums.length === 0) {
       return [];
