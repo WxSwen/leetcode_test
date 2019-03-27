8e788e17ea1955692c5e3cb4cd4fd3bd49e1f024
WxSwen <511854671@qq.com>
2019-03-27 11:04:33 +0800

    feat: 修改32

--- 32.js
+++ 32.js
@@ -16,45 +16,79 @@
  * @param {string} s
  * @return {number}
  */
-var longestValidParentheses = function(s, max) {
-  let stack = [];
-  let copyS = [];
+// var longestValidParentheses = function(s, max) {
+//   let stack = [];
+//   let copyS = [];
 
-  s.split('').forEach((str, index) => {
-    if(str === '(') {
-      stack.push({
-        str,
-        index
-      });
+//   s.split('').forEach((str, index) => {
+//     if(str === '(') {
+//       stack.push({
+//         str,
+//         index
+//       });
+//     } else {
+//       if(stack[stack.length - 1] && '(' === stack[stack.length - 1].str) {
+//         let start = stack.pop();
+//         copyS[start.index] = 1;
+//         copyS[index] = 1;
+//       } else {
+//         return;
+//       }
+//     }
+//   });
+//   return findTheMostLongArray(copyS);
+// }
+
+// function findTheMostLongArray(arr) {
+//   let max = 0;
+//   let i = 0;
+//   let tempMax = 0;
+
+//   while(i <= arr.length) {
+//     if(arr[i] === 1) {
+//       i += 2;
+//       tempMax += 2;
+//     } else {
+//       i++;
+//       max = max > tempMax ? max : tempMax;
+//       tempMax = 0;
+//     }
+//   }
+//   return max;
+// }
+
+var longestValidParentheses = function(s) {
+  var max = 0;
+  var stack = [-1];
+
+  for(let i = 0; i < s.length; i++) {
+    if('(' === s[i]) {
+      stack.push(s[i]);
     } else {
-      if(stack[stack.length - 1] && '(' === stack[stack.length - 1].str) {
-        let start = stack.pop();
-        copyS[start.index] = 1;
-        copyS[index] = 1;
-      } else {
-        return;
-      }
-    }
-  });
-  return findTheMostLongArray(copyS);
-}
-
-function findTheMostLongArray(arr) {
-  let max = 0;
-  let i = 0;
-  let tempMax = 0;
-
-  while(i <= arr.length) {
-    if(arr[i] === 1) {
-      i += 2;
-      tempMax += 2;
-    } else {
-      i++;
-      max = max > tempMax ? max : tempMax;
-      tempMax = 0;
+      stack.pop();
+      if(!stack.length) stack.push(i);
+      // stack.push(i)
+      // i - stack[stack.length - 1]
+      else max = Math.max(max, i - stack[stack.length - 1]);
     }
   }
   return max;
-}
+};
 
-console.log(longestValidParentheses("(()()()()()(((()))()()()()))"));
+// var longestValidParentheses = function(s) {
+//   let longest = 0;
+//   let stack = [-1];
+//   for (let i = 0; i < s.length; i++) {
+//     if (s[i] === '(') {
+//       stack.push(i);
+//     } else {
+//       stack.pop();
+//       if (!stack.length) stack.push(i);
+//       else longest = Math.max(longest, i - stack[stack.length - 1]);
+//     }
+//   }
+
+//   return longest;
+// };
+
+console.log(longestValidParentheses(")))()()()()()"));
