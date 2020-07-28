76560a5d7ed92789f35740a6ca1e232d3cf2969d
WxSwen <511854671@qq.com>
2020-07-28 14:47:22 +0800

    feat: 添加路径查找

--- 剑指 Offer 04. 二维数组中的查找.js
+++ 剑指 Offer 04. 二维数组中的查找.js
@@ -3,9 +3,19 @@
  * @param {number} target
  * @return {boolean}
  */
-var findNumberIn2DArray = function(matrix, target) {
-  
-  while () { 
-    
+var findNumberIn2DArray = function (matrix, target) {
+  let rows = 0;
+  let column = matrix.length - 1;
+
+  while (column >= 0 && rows <= matrix[0].length - 1) {
+    if (matrix[column][rows] > target) {
+      column--;
+    } else if (matrix[column][rows] < target) {
+      rows++;
+    } else {
+      return true;
+    }
   }
+  return false;
 };
+console.log(findNumberIn2DArray([[-5]], -5));
